bf73663498e42d583e56e9fce1b4b2d3dd847f5d
stepkos <stepkos@wp.pl>
2023-03-20 09:17:54 +0100

    SJF - only another comparator than FCFS

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import algorithms.FCFS;
+import algorithms.SJF;
 import process.Process;
 import process.ProcessList;
 
@@ -7,9 +8,13 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        ProcessList processList = new ProcessList(20, 123);
-        FCFS fcfs = new FCFS(processList);
+        ProcessList processList1 = new ProcessList(2000, 123);
+        FCFS fcfs = new FCFS(processList1);
         fcfs.execute();
+
+        ProcessList processList2 = new ProcessList(2000, 123);
+        SJF sjf = new SJF(processList2);
+        sjf.execute();
 
 
         // TODO ide czasu do rozpoczecia lub od statniego wykonania
---
+++ src/main/java/algorithms/SJF.java
@@ -0,0 +1,47 @@
+package algorithms;
+
+import process.Process;
+import process.ProcessList;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SJF {
+    private final ProcessList processList;
+    private int executionTime = 0;
+    private int breakTime = 0;
+    private int summaryWaitingTime = 0;
+    private int maxWaitingTime = 0;
+
+    public SJF(ProcessList processList) {
+        this.processList = processList;
+        this.processList.sort(Comparator.comparingInt(Process::getTotalTime));
+    }
+
+    public void execute() {
+        while (!processList.isDone()) {
+            List<Process> actualList = processList.getExistsAndNotDoneList(executionTime + breakTime);
+            if (actualList.isEmpty())
+                breakTime++;
+            else {
+                // TODO Debug summaryWaitingTime for spec list
+                int waitingTime = executionTime + breakTime - actualList.get(0).getArrivalTime();
+                if (waitingTime > maxWaitingTime) maxWaitingTime = waitingTime;
+                summaryWaitingTime += waitingTime;
+                executionTime += actualList.get(0).execute();
+            }
+        }
+
+        // Temporary stats
+        int listSize = processList.getList().size();
+        System.out.println("STATS FOR SJF");
+        System.out.println("Process amount: " + listSize);
+        System.out.println("Change content amount: " + listSize);
+        System.out.println("Execution time: " + executionTime);
+        System.out.println("Break time: " + breakTime);
+        System.out.println("Avg waiting time: " + ((listSize > 0) ? summaryWaitingTime / listSize : "No processes"));
+        System.out.println("Max waiting time: " + maxWaitingTime);
+        System.out.println();
+    }
+
+}
