8f0bdf4aaabd6d22284b959d754ccdd52ba36092
stepkos <stepkos@wp.pl>
2023-03-22 00:04:51 +0100

    Convert Statistic to record and

--- src/main/java/statistics/Statistic.java
+++ src/main/java/statistics/Statistic.java
@@ -1,52 +1,7 @@
 package statistics;
 
-public class Statistic {
-
-    private final int processAmount;
-    private final int changeContentAmount;
-    private final int executionTime;
-    private final int breakTime;
-    private final int avgWaitingTime;
-    private final int maxWaitingTime;
-    private final int avgWaitingForFirstActionTime;
-
-    public Statistic(int processAmount, int changeContentAmount, int executionTime, int breakTime, int avgWaitingTime, int maxWaitingTime, int avgWaitingForFirstActionTime) {
-        this.processAmount = processAmount;
-        this.changeContentAmount = changeContentAmount;
-        this.executionTime = executionTime;
-        this.breakTime = breakTime;
-        this.avgWaitingTime = avgWaitingTime;
-        this.maxWaitingTime = maxWaitingTime;
-        this.avgWaitingForFirstActionTime = avgWaitingForFirstActionTime;
-    }
-
-    public int getProcessAmount() {
-        return processAmount;
-    }
-
-    public int getChangeContentAmount() {
-        return changeContentAmount;
-    }
-
-    public int getExecutionTime() {
-        return executionTime;
-    }
-
-    public int getBreakTime() {
-        return breakTime;
-    }
-
-    public int getAvgWaitingTime() {
-        return avgWaitingTime;
-    }
-
-    public int getMaxWaitingTime() {
-        return maxWaitingTime;
-    }
-
-    public int getAvgWaitingForFirstActionTime() {
-        return avgWaitingForFirstActionTime;
-    }
+public record Statistic(int processAmount, int changeContentAmount, int executionTime,
+        int breakTime, int avgWaitingTime, int maxWaitingTime, int avgWaitingForFirstActionTime) {
 
     @Override
     public String toString() {
