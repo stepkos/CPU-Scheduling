8f1c4c69fa5ef1aeb354f2790aed6660b1df7583
stepkos <stepkos@wp.pl>
2023-03-23 18:58:03 +0100

    Cosmetic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         new StatisticPresenter().demo();
-//        new StatisticPresenter(20_000, 10, 200_000, 1, 30, 200_000).demo();
 
     }
 }
